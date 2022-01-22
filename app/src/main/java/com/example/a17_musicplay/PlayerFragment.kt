5c8a2f4991f71802e39a5af0d0bce4eab229c6ca
Songmi <ksongmi0727@gmail.com>
2022-01-22 23:48:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a17_musicplay/PlayerFragment.kt
@@ -0,0 +1,19 @@
+package com.example.a17_musicplay
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class PlayerFragment: Fragment(R.layout.fragment_player) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object{
+        fun newInstance(): PlayerFragment {
+            return PlayerFragment()
+        }
+    }
+
+}
